037c232a1705282698f3f41a55166a541db0908a
Felix Vigneault <minivigno@gmail.com>
2018-10-06 14:10:09 -0400

    Added navigation helper

---
+++ LHGames/Interfaces/INavigationHelper.cs
@@ -0,0 +1,15 @@
+﻿using LHGames.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LHGames.Interfaces
+{
+    interface INavigationHelper
+    {
+        int HowManyTurnsToFollowPath(List<Tile> path);
+
+        string NavigateToNextPosition(Tile adjacentTile);
+    }
+}
