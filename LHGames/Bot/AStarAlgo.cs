dfa12b398e6f29a541145916ade19e56725beef6
jeepee12 <jeepee.12@hotmail.com>
2018-10-06 14:05:21 -0400

    Can pass trought walls

--- LHGames/Bot/AStarAlgo.cs
+++ LHGames/Bot/AStarAlgo.cs
@@ -114,7 +114,7 @@ namespace LHGames.Bot
                     {
                         tile = neighbor,
                         parent = current_tuple,
-                        real_cost_to_get_here = current_tuple.real_cost_to_get_here + 1,
+                        real_cost_to_get_here = current_tuple.real_cost_to_get_here + GetCostToWalkUponTile(neighbor),
                         heuristique_remaining = heuristique(neighbor.Position, end.Position)
                     };
 
@@ -236,7 +236,7 @@ namespace LHGames.Bot
         public bool IsTileWalkable(TileContent tileContent)
         {
             // TODO handle lava
-            return tileContent == TileContent.Empty || tileContent == TileContent.House;
+            return tileContent == TileContent.Empty || tileContent == TileContent.House || (map.WallsAreBreakable && tileContent == TileContent.Wall);
         }
     }
 }
