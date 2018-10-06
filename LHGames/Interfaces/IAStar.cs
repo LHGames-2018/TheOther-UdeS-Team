3ed7deb37455d3b939e57590cd600571303c96f8
Felix Vigneault <minivigno@gmail.com>
2018-10-06 11:17:07 -0400

    Added interface for resource planner

---
+++ LHGames/Interfaces/IAStar.cs
@@ -0,0 +1,13 @@
+﻿using LHGames.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LHGames.Interfaces
+{
+    public interface IAStar
+    {
+        List<Tile> Run(Tile start, Tile end);
+    }
+}
